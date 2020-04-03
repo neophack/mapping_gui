d5ffae3c3b9df42eb23433ea60a375badceb8446
Linsin <jianglingxin@aliyun.com>
2020-04-03 17:50:10 +0800

    feat: community with memory

--- include/LoopClosurePane.h
+++ include/LoopClosurePane.h
@@ -36,8 +36,28 @@ class LoopClosurePane {
   // Getter for the information and the usage
  private:
 
+  void InitMemory();
+  void NewManual();
+
   const float m_fItemSpacing;
   bool accept_loop = false;
+  bool init_memory = false;
+
+  int flag_address = 0;
+  int opt_address = 0;
+  int start_address = 0;
+  int end_address = 0;
+  int near_address = 0;
+  int max_address = 0;
+  int accept_address = 0;
+
+  char *flag_text = nullptr;
+  char *opt_text = nullptr;
+  char *start_text = nullptr;
+  char *end_text = nullptr;
+  char *near_text = nullptr;
+  char *max_text = nullptr;
+  char *accept_text = nullptr;
 
 };
 }
--- src/LoopClosurePane.cpp
+++ src/LoopClosurePane.cpp
@@ -1,7 +1,12 @@
 //
 // Created by linsin on 20/03/2020.
 //
-
+#include <stdio.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "MainWindow.h"
 #include "LoopClosurePane.h"
 panorama::LoopClosurePane::LoopClosurePane()
@@ -11,7 +16,41 @@ panorama::LoopClosurePane::LoopClosurePane()
 panorama::LoopClosurePane::~LoopClosurePane() {
 }
 
+void panorama::LoopClosurePane::NewManual() {
+  std::string ret = BashExec("gnome-terminal -t \"Launch LEGO\" -x bash -c \"roslaunch lego_loam run.launch;exec bash; \" ");
+}
+
+void panorama::LoopClosurePane::InitMemory() {
+  flag_address = shmget(IPC_PRIVATE, 129, IPC_CREAT | 0644);
+  opt_address = shmget(IPC_PRIVATE, 129, IPC_CREAT | 0644);
+  start_address = shmget(IPC_PRIVATE, 129, IPC_CREAT | 0644);
+  end_address = shmget(IPC_PRIVATE, 129, IPC_CREAT | 0644);
+  near_address = shmget(IPC_PRIVATE, 129, IPC_CREAT | 0644);
+  max_address = shmget(IPC_PRIVATE, 129, IPC_CREAT | 0644);
+  accept_address = shmget(IPC_PRIVATE, 129, IPC_CREAT | 0644);
+  flag_text = (char*)shmat(flag_address, NULL , 0);
+  opt_text = (char*)shmat(opt_address, NULL , 0);
+  start_text = (char*)shmat(start_address, NULL , 0);
+  end_text = (char*)shmat(end_address, NULL , 0);
+  near_text = (char*)shmat(near_address, NULL , 0);
+  max_text = (char*)shmat(max_address, NULL , 0);
+  accept_text = (char*)shmat(accept_address, NULL , 0);
+  strcpy(flag_text, "false");
+  strcpy(opt_text, "@opt");
+  strcpy(start_text, "@start");
+  strcpy(end_text, "@end");
+  strcpy(near_text, "@near");
+  strcpy(max_text, "@max");
+  strcpy(accept_text, "@accept");
+
+  printf("%d %d %d %d %d %d %d\n", flag_address, opt_address, start_address, end_address, near_address, max_address, accept_address);
+  init_memory = true;
+}
+
 void panorama::LoopClosurePane::renderUI() {
+  if(!init_memory) {
+    InitMemory();
+  }
   ImGui::BeginChild("##loopclosurePane");
 
   ImGui::PushFont(panorama::getFont(PANORAMA_FONT_TITLE));
@@ -32,6 +71,12 @@ void panorama::LoopClosurePane::renderUI() {
 
 
   if (ImGui::Button("Loop these frames..")) {
+    strcpy(start_text, past_frame);
+    strcpy(end_text, current_frame);
+    strcpy(near_text, number_of_frame);
+    strcpy(max_text, max_distance);
+    strcpy(opt_text, "loop");
+    strcpy(flag_text, "true");
     ImGui::OpenPopup("Accept?");
   }
   bool opt_click_accept = false;
@@ -39,16 +84,21 @@ void panorama::LoopClosurePane::renderUI() {
     ImGui::Text("Checkout the point cloud window.\nDo u want to accept this loop closure!\n\n");
     ImGui::Separator();
 
-    if (ImGui::Button("Sure!", ImVec2(120, 0))) {accept_loop = true;opt_click_accept = true; ImGui::CloseCurrentPopup(); }
+    if (ImGui::Button("Sure!", ImVec2(120, 0))) {
+      strcpy(accept_text, "Y");
+      accept_loop = true;opt_click_accept = true; ImGui::CloseCurrentPopup();
+    }
     ImGui::SetItemDefaultFocus();
     ImGui::SameLine();
-    if (ImGui::Button("Hell u r", ImVec2(120, 0))) {accept_loop = false; opt_click_accept = true; ImGui::CloseCurrentPopup(); }
+    if (ImGui::Button("Hell u r", ImVec2(120, 0))) {
+      strcpy(accept_text, "N");
+      accept_loop = false; opt_click_accept = true; ImGui::CloseCurrentPopup();
+    }
     ImGui::EndPopup();
   }
   if(opt_click_accept ) {
     if( accept_loop) {
       std::cout << "sure\n";
-      std::cout << current_frame << std::endl;
     } else{
       std::cout << "hell u r\n";
     }
@@ -60,6 +110,8 @@ void panorama::LoopClosurePane::renderUI() {
   ImGui::PopFont();
 
   if(ImGui::Button("Rebuild the map")) {
+    strcpy(opt_text, "r");
+    strcpy(flag_text, "true");
     std::cout << "Rebuild the map\n";
   }
 
@@ -78,6 +130,12 @@ void panorama::LoopClosurePane::renderUI() {
 
   if(ImGui::Button("delete..")) {
     std::cout << "delete\n";
+  }
+
+  ImGui::Separator();
+  if(ImGui::Button("Finished!!!")) {
+    strcpy(opt_text, "e");
+    strcpy(flag_text, "true");
   }
 
 
